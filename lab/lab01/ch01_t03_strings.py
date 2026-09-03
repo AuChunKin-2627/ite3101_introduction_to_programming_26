0f7560d57b48589fcaa925b260bbbb1076528505
AuChunKin-2627 <260483253@stu.vtc.edu.hk>
2026-09-03 08:40:15 +0000

    [skip actions] lab/lab01/ch01_t03_strings.py

--- lab/lab01/ch01_t03_strings.py
+++ lab/lab01/ch01_t03_strings.py
@@ -1,3 +1,3 @@
-print("Thisis a good sting")
-print("You can use single quotes or double quotes for a sting")
+print("Thisis a good string")
+print("You can use single quotes or double quotes for a string")
 print("This is"+"a good string")
