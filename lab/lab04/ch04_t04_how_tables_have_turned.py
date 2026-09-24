18b548a19f186ab65ab605387478ce2fb4c2f85f
AuChunKin-2627 <260483253@stu.vtc.edu.hk>
2026-09-24 08:29:02 +0000

    [skip actions] lab/lab04/ch04_t04_how_tables_have_turned.py

--- lab/lab04/ch04_t04_how_tables_have_turned.py
+++ lab/lab04/ch04_t04_how_tables_have_turned.py
@@ -7,7 +7,7 @@ bool_one = 3 < 5  # We already did this one for you!
 bool_two = 52 > 300
 
 # Make me true!
-bool_three = 32 <= 
+bool_three = 32 <= 64
 
 # Make me false!
 bool_four = 2036/22 <= -10**10
