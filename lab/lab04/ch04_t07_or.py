fc5e152b892b66e8a11230115dcc788fc48d308d
AuChunKin-2627 <260483253@stu.vtc.edu.hk>
2026-09-24 08:42:18 +0000

    [skip actions] lab/lab04/ch04_t07_or.py

--- lab/lab04/ch04_t07_or.py
+++ lab/lab04/ch04_t07_or.py
@@ -8,16 +8,17 @@ bool_four = True
 
 bool_five = True
 
-a=2 ** 3 == 108 % 100 or 'Cleese' == 'King Arthur'
-b=True or False
-c=100 ** 0.5 >= 50 or False
-d=True or True
-e=1 ** 100 == 100 ** 1 or 3 * 2 * 1 != 3 + 2 + 1
-f=3 * 2 * 1 != 3 + 2 + 1
-g=1 ** 100 == 100 ** 1
+a = 2 ** 3 == 108 % 100 or 'Cleese' == 'King Arthur'
+b = True or False
+c = 100 ** 0.5 >= 50 or False
+d = True or True
+e = 1 ** 100 == 100 ** 1 or 3 * 2 * 1 != 3 + 2 + 1
+f = 3 * 2 * 1 != 3 + 2 + 1
+g = 1 ** 100 == 100 ** 1
 print(a)
 print(b)
 print(c)
 print(d)
 print(e)
 print(f)
+print(g)
